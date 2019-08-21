dea302c4e8f47a3c5cad885d81d28087716d1377
ISAAC <ibcho1@student.monash.edu>
2019-08-21 15:57:32 +1000

    Iteration 2 Week 5

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 //npm install body-parser --save
 
-//Create Instance
-let express = require('express');
-let bodyParser = require('body-parser');
-let app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -15,9 +11,4 @@ app.use(bodyParser.json())
 app.get('/', function (rqe, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.post('/data', function (req, res) {
-    console.log(req.body.username);
-    console.log(req.body.userage);
-    res.send('Thank You')
-})
 app.listen(8080);
