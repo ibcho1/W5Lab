df0fcecdcc0cd5de990061cafb1780b62f9c66d2
ISAAC <ibcho1@student.monash.edu>
2019-08-21 16:45:52 +1000

    I3

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ app.use(express.static('css'));
 
 app.get('/', function (req, res) {
     let randomId = Math.round(Math.random() * 100);
-    res.render('index.html', { username: "admin",id: randomId});
+    res.render('index.html', {
+        username: "admin",
+        id: randomId
+    });
 });
+
+app.get('/addTask.html', function (req, res) {
+    console.log("addTask");
+    res.render('addTask.html');
+});
+
+app.get('/listTask.html', function (req, res) {
+    console.log("listTask");
+    res.render('listTask.html');
+});
+
+
 app.listen(8080);
