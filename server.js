5de8aa14c8e71b955c594d7476857bc44a790811
ISAAC <ibcho1@student.monash.edu>
2019-08-26 19:44:19 +1000

    Monday 26th August 8pm

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+let viewsPath = __dirname + "/views/";
+let db = [];
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-
 app.use(express.static('images'));
 app.use(express.static('css'));
-
 
 
 app.get('/', function (req, res) {
@@ -17,14 +20,26 @@ app.get('/', function (req, res) {
     });
 });
 
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.get('/addTask.html', function (req, res) {
     console.log("addTask");
-    res.render('addTask.html');
+    res.sendFile(viewsPath + 'addTask.html');
+});
+
+app.post('/addThisTask', function(req, res){
+   console.log(req.body);
+   db.push(req.body); 
+   res.render(viewsPath + 'listTask.html', {
+       task: db
+    });
 });
 
 app.get('/listTask.html', function (req, res) {
     console.log("listTask");
-    res.render('listTask.html');
+    res.render(viewsPath + 'listTask.html', {
+        task: db
+    });
 });
 
 
